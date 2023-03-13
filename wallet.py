eaba03202442e0034c6435a0e3b33ddfe85d1d83
N <125567191+Gaoning1121@users.noreply.github.com>
2023-03-13 08:33:15 +0800

    Add files via upload

---
+++ wallet.py
@@ -0,0 +1,17 @@
+from eth_keys import keys
+from eth_utils import decode_hex
+import os
+
+n = 570  # 生成的钱包数
+wallet_file = "wallet2.txt"
+wallets = []
+
+for i in range(n):
+    private_key = keys.PrivateKey(os.urandom(32))
+    public_key = private_key.public_key
+    address = keys.PublicKey(public_key.to_bytes()).to_checksum_address()
+    wallets.append([address,private_key.to_hex()])
+  
+with open(wallet_file, mode='w') as file:
+    for wallet in wallets:
+        file.write(wallet[0] + "," + wallet[1] + "\n")
